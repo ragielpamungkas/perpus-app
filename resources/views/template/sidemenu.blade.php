9fe8885b7e8536e1de3ce76a0bc80d99ed1a871d
Ragiel Pamungkas <kr.pamungkas39@gmail.com>
2022-08-20 01:18:52 +0700

    change menu title data pengguna to data pengguna perpus

--- resources/views/template/sidemenu.blade.php
+++ resources/views/template/sidemenu.blade.php
@@ -22,7 +22,7 @@
                   <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                     <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
                   </div>
-                  <span class="nav-link-text ms-1">Data Pengguna</span>
+                  <span class="nav-link-text ms-1">Data Pengguna Perpustakaan</span>
                 </a>
               </li>
           </li>
